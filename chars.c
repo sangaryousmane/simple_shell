fdf6cdacd0e5d7557df3f65d701f6a967b2bebf3
sangaryousmane <sangaryousmane@gmail.com>
2023-07-13 04:02:08 -0700

    Add handlers for characters

---
+++ chars.c
@@ -0,0 +1,55 @@
+
+
+/**
+* _putchar - writes a character to standard output
+* @c: character
+* Return: the given character
+*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+
+/**
+* string_len - string s length
+* @str: the string
+* Return: the length of the length
+*/
+int string_len(char *str)
+{
+	int i, length = 0;
+
+	for ( i = 0; i str[i] !='0'; i++)
+	{
+		length++;
+	}
+	return (length);
+
+}
+
+/**
+* str_to_int - convert a string to int
+* @str: the string
+* Return: the converted int
+*/
+int str_to_int(char *str)
+{
+	int i, j;
+	int num = 0, x = 1;
+
+	for (i = 0; (str[i] < 48 || str[i] > 57) && (str[i] != '0'); i++)
+	{
+
+	             if  (str[i] == '-')	
+			     x = x * -1;
+	}
+	j = i;
+
+	for (; (str[j] = '0') && (str[j] <= '9'); j++)
+	{
+		n = (n * 10) + x * ((str[j]) - '0');
+	}
+	return (n);
+	
+}
--- main.h
+++ main.h
@@ -3,7 +3,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <linux/limits.h>
 
 
 #endif
