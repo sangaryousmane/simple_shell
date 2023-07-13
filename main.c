28b78e3a4939f88ced738e8531dc95f21567d5ec
sangaryousmane <sangaryousmane@gmail.com>
2023-07-13 03:32:17 -0700

    Add main function as entry point and a betty style compliant

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+/**
+* main - entry point for the simple shell
+* @argc: counts of argument
+* @argv: arguments values
+* @Return: exit status
+*
+*/
+int main(int argc, char **argv)
+{
+	printf("Hello world");
+}
