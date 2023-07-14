861a0319d0892eaec80da0161f2d7c72b6deefa8
sangaryousmane <sangaryousmane@gmail.com>
2023-07-14 07:13:32 -0700

    Add functions to main.h

--- main.h
+++ main.h
@@ -18,5 +18,8 @@ int _putchar(char c);
 int str_to_int(char *str);
 int string_len(char *str);
 void _remove(char *b);
+char *_memcopy(char *d, char src, unsigned int n);
+void *_fill(void *arr, int element, unsigned int len);
+void *_customer_realloc(void *to, unsigned int pre_size, unsigned int current_size);
 
 #endif
