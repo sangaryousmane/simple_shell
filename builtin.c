5921abeb7d58ec170c2b9039c2a03cfff49efd6e
Ousmane <88728076+sangaryousmane@users.noreply.github.com>
2023-07-25 20:43:13 +0800

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -15,10 +15,10 @@ char *pwd =  getenv("PWD");
 status = 1;
 
 if (!command[1])
-	status = chdir(getenv("$HOME"));
+	status = chdir(getenv("HOME"));
 else if (str_cmp(command[1], "-") == 0)
 {
-	status = chdir("OPWD");
+	status = chdir(getenv("OLDPWD"));
 }
 else
 {
@@ -27,13 +27,13 @@ status = chdir(command[1]);
 
 if (status == -1)
 {
-perror("$hsh");
+perror("hsh");
 return (-1);
 }
-else
+else if (status != -1)
 {
 getcwd(cwd, sizeof(cwd));
-setenv("OPWD", pwd, 1);
+setenv("OLDPWD", pwd, 1);
 setenv("PWD", cwd, 1);
 }
 return (0);
@@ -79,7 +79,7 @@ int _echo(char **command, int error)
     unsigned int process_id = getppid();
 
     if (_strncompare(command[1], "$?", 2) == 0) {
-        print_number_in(error);
+        _char_int(error);
         DISPLAY_TO_STDOUT("\n");
     } else if (_strncompare(command[1], "$$", 2) == 0) {
         _int_print(process_id);
