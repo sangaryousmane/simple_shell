50adcd0eabcd68c4dfd227b86aff2b3fe9cce86d
Ousmane <88728076+sangaryousmane@users.noreply.github.com>
2023-07-27 23:40:17 +0800

    Update _strtok.c

--- _strtok.c
+++ _strtok.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
+/**
+ * print_number_in -Print Number Putchar
+ * @n:Integer
+ * Return: void
+ */
+void print_number_in(int n)
+{
+	unsigned int x = n;
 
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -x;
+	}
+	if ((x / 10) > 0)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
+}
 
 /**
  * _remove - removes all after #
