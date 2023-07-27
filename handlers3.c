712c84594bb4df25c6bf0c2bc155e8df9e930faa
Ousmane <88728076+sangaryousmane@users.noreply.github.com>
2023-07-27 23:36:57 +0800

    Update handlers3.c

--- handlers3.c
+++ handlers3.c
@@ -13,10 +13,45 @@ void _custom_error(char *input, int count, char **argv)
 
 	DISPLAY_TO_STDOUT(argv[0]);
 	DISPLAY_TO_STDOUT(": ");
-	error = _itoa(count);
+	error = _int_char(count);
 	DISPLAY_TO_STDOUT(error);
 	DISPLAY_TO_STDOUT(error);
 	DISPLAY_TO_STDOUT(": ");
 	DISPLAY_TO_STDOUT(input);
 	PRINTER(": not found\n");
+}
+
+
+#include "main.h"
+
+/**
+* handle_display - Execute Normal Echo
+* @command: Parsed Command
+* Return: 0 on Succes -1 on Fail
+*/
+int handle_display(char **command)
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == 0)
+	{
+	if (execve("/bin/echo", command, environ) == -1)
+	{
+		return (-1);
+	}
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		do {
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	return (1);
 }
