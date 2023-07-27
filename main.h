a23801738d8f1261afc692a4991d716c29d0cc72
Ousmane <88728076+sangaryousmane@users.noreply.github.com>
2023-07-27 22:51:06 +0800

    Update main.h

--- main.h
+++ main.h
@@ -35,7 +35,7 @@ typedef struct shell_builtin  _command;
 /** utility commands **/
 int _putchar(char c);
 int str_to_int(char *str);
-int string_len(char *str);
+int _strlen(char *str);
 void _remove(char *b);
 char *_memcopy(char *d, char *src, unsigned int n);
 void *_fill(void *arr, int element, unsigned int len);
