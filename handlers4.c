e8d6e5dcea25eaf5bfe715e6d1f8e197f5304306
Ousmane <88728076+sangaryousmane@users.noreply.github.com>
2023-07-27 23:37:39 +0800

    Update handlers4.c

--- handlers4.c
+++ handlers4.c
@@ -1,42 +1,5 @@
 #include "main.h"
 
-
-/**
- *  _error - customize errors
- * @argv: program's name
- * @count: count errors
- * @command: the command
- * Return: nothing
- */
-void _error(char **argv, int count, char **command)
-{
-        char *error = _itoa(count);
-
-        DISPLAY_TO_STDOUT(argv[0]);
-        DISPLAY_TO_STDOUT(": ");
-        DISPLAY_TO_STDOUT(error);
-        DISPLAY_TO_STDOUT(": ");
-        DISPLAY_TO_STDOUT(command[0]);
-        DISPLAY_TO_STDOUT(": Illegal number: ");
-        DISPLAY_TO_STDOUT(command[1]);
-        DISPLAY_TO_STDOUT("\n");
-        free(error);
-}
-
-/**
- * print_number -Print Unsigned Int Putchar
- * @n: Unisigned Integer
- * Return: Void
- */
-void print_number(unsigned int n)
-{
-	unsigned int x = n;
-
-	if ((x / 10) > 0)
-		print_number(x / 10);
-
-	_putchar(x % 10 + '0');
-}
 /**
  * print_number_in -Print Number Putchar
  * @n:Integer
