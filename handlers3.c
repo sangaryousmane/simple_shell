bf91fa60da9c48f600e8a524cb3d04c1bc9f7380
Ousmane <88728076+sangaryousmane@users.noreply.github.com>
2023-07-27 23:38:30 +0800

    Update handlers3.c

--- handlers3.c
+++ handlers3.c
@@ -21,9 +21,6 @@ void _custom_error(char *input, int count, char **argv)
 	PRINTER(": not found\n");
 }
 
-
-#include "main.h"
-
 /**
 * handle_display - Execute Normal Echo
 * @command: Parsed Command
@@ -54,4 +51,45 @@ int handle_display(char **command)
 		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
 	}
 	return (1);
+}
+
+
+
+
+
+/**
+ *  _error - customize errors
+ * @argv: program's name
+ * @count: count errors
+ * @command: the command
+ * Return: nothing
+ */
+void _error(char **argv, int count, char **command)
+{
+        char *error = _itoa(count);
+
+        DISPLAY_TO_STDOUT(argv[0]);
+        DISPLAY_TO_STDOUT(": ");
+        DISPLAY_TO_STDOUT(error);
+        DISPLAY_TO_STDOUT(": ");
+        DISPLAY_TO_STDOUT(command[0]);
+        DISPLAY_TO_STDOUT(": Illegal number: ");
+        DISPLAY_TO_STDOUT(command[1]);
+        DISPLAY_TO_STDOUT("\n");
+        free(error);
+}
+
+/**
+ * print_number -Print Unsigned Int Putchar
+ * @n: Unisigned Integer
+ * Return: Void
+ */
+void print_number(unsigned int n)
+{
+	unsigned int x = n;
+
+	if ((x / 10) > 0)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
 }
